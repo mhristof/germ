481f3c0c0ef03bf0bc69eee6e029a46ef5a9f806
Mike Christofilopoulos <mhristof@gmail.com>
2022-08-12 14:57:38 +0300

    fix(ssr): fix terraform resources docs redirects

--- iterm/ssr.go
+++ iterm/ssr.go
@@ -43,7 +43,7 @@ func SmartSelectionRules(custom string) []SmartSelectionRule {
 				SmartSelectionRuleAction{
 					Title:     "open webpage",
 					Action:    1,
-					Parameter: `https://www.terraform.io/docs/providers/aws/r/\1`,
+					Parameter: `https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/\1`,
 				},
 			},
 		},
@@ -55,7 +55,7 @@ func SmartSelectionRules(custom string) []SmartSelectionRule {
 				{
 					Title:     "open webpage",
 					Action:    1,
-					Parameter: "https://www.terraform.io/docs/providers/aws/d/\\1",
+					Parameter: `https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/\1`,
 				},
 			},
 		},
